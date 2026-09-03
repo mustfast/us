99773fd595a5936d2600bb6ca410e47dc5be115d
LiuLiming <758185271@qq.com>
2026-09-04 00:14:59 +0800

    Keep only built files

--- src/components/SelfAssessment.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="section">
-    <h2>自我评价</h2>
-    <div class="entry-desc">
-      <p>以工程师的思维，追求工程上的确定性，做事习惯从产品角度出发，先后落地多套系统，显著提升了美术团队的工作效率；同时保持对新技术的敏感，正从 AI 的视角重新定义公司资产管线，构建符合 AI 思维的资产中心。</p>
-    </div>
-  </section>
-</template>
